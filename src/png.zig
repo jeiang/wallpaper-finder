2d0617492f3c286cfd0de871a160d0421c0e1126
Aidan Pinard <aidan@aidanpinard.co>
2024-03-02 23:32:55 -0400

    refactor: stop doing manual parsing for png, use bitcast instead

--- src/png.zig
+++ src/png.zig
@@ -5,8 +5,8 @@ const utils = @import("utils.zig");
 const Dimensions = utils.Dimensions(u32);
 
 const logger = std.log.scoped(.png_parser);
-const png_signature = [_]u8{ 137, 80, 78, 71, 13, 10, 26, 10 };
-const expected_ihdr_marker = "IHDR";
+const png_signature = std.mem.readInt(u64, &[_]u8{ 137, 80, 78, 71, 13, 10, 26, 10 }, .little);
+const expected_ihdr_marker = std.mem.readInt(u32, "IHDR", .little);
 
 pub const extensions = .{".png"};
 
@@ -14,37 +14,42 @@ pub const PngError = error{
     MissingIHDR,
 } || utils.ImageError;
 
+// should be 24 bytes
+const PngStartingBytes = packed struct {
+    signature: u64, // 8
+    chunk_len: u32, // 4
+    header: u32, // 4
+    width: u32, // 4
+    height: u32, // 4
+};
+
 /// See https://stackoverflow.com/a/16725066
 pub fn getSize(file: *fs.File) !Dimensions {
-    var buf = [_]u8{0} ** (png_signature.len + @sizeOf(u32) + expected_ihdr_marker.len + @sizeOf(u32) * 2);
+    var buf = [_]u8{0} ** @sizeOf(PngStartingBytes);
     const bytes_read = try file.readAll(&buf);
     if (bytes_read < buf.len) {
         return PngError.ReachedEof;
     }
 
-    const signature = buf[0..png_signature.len];
-    if (!std.mem.eql(u8, &png_signature, signature)) {
+    const starting_bytes: PngStartingBytes = @bitCast(buf);
+
+    if (starting_bytes.signature != png_signature) {
         logger.warn("file does not have a png signature, expected {X} got {X}", .{
             png_signature,
-            signature,
+            starting_bytes.signature,
         });
         return PngError.MissingSignature;
     }
-    const ihdr_marker = buf[(png_signature.len + @sizeOf(u32))..(png_signature.len + @sizeOf(u32) + expected_ihdr_marker.len)];
-    if (!std.mem.eql(u8, expected_ihdr_marker, ihdr_marker)) {
-        logger.warn("png does not have IHDR at the expected position, expected {s} got {s}", .{
+    if (starting_bytes.header != expected_ihdr_marker) {
+        logger.warn("png does not have the IHDR chunk name at the expected position, expected {X} got {X}", .{
             expected_ihdr_marker,
-            ihdr_marker,
+            starting_bytes.header,
         });
         return PngError.MissingIHDR;
     }
 
-    const start_idx = @sizeOf(u32) + png_signature.len + expected_ihdr_marker.len;
-    const height = std.mem.readInt(u32, buf[(start_idx + 4)..buf.len], .big);
-    const width = std.mem.readInt(u32, buf[start_idx..(start_idx + 4)], .big);
-
     return Dimensions{
-        .height = height,
-        .width = width,
+        .height = starting_bytes.height,
+        .width = starting_bytes.width,
     };
 }
